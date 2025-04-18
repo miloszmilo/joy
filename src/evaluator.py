6a781175ac5de6ebaaddccb2da461dd1e43d6a14
NxtPerfect <alakaxan@protonmail.com>
2025-04-18 11:35:15 +0200

    feat: added evaluate public function

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,76 +1,11 @@
 from collections import deque
-from enum import EnumType
 from typing import Callable, override
 
 from src.exceptions.ExpressionError import ExpressionError
+from src.joyTypes.Symbol import Symbol, SymbolType, binary_operators, unary_operators
 from src.joyTypes.Token import Token
 
 MAX_PRECEDENCE = 100
-
-
-class SymbolType(EnumType):
-    NUMBER: str = "number"
-    OPERATOR: str = "operator"
-    PARENTHESIS_OPEN: str = "parenthesis_open"
-    PARENTHESIS_CLOSE: str = "parenthesis_close"
-    UNKNOWN: None = None
-
-
-class Symbol:
-    value: str
-    type: str | None
-    argument_count: int
-    precedence: int
-
-    def __init__(
-        self,
-        value: str = "",
-        type: str | None = SymbolType.UNKNOWN,
-        argument_count: int = 2,
-        precedence: int = -1,
-    ) -> None:
-        self.value = value
-        self.type = type
-        self.argument_count = argument_count
-        _precedence = precedence
-        if precedence == -1:
-            _precedence = binary_operators.get(value, -1)
-            if isinstance(_precedence, Symbol):
-                _precedence = _precedence.precedence
-        self.precedence = _precedence
-
-    @override
-    def __eq__(self, value: object, /) -> bool:
-        if isinstance(value, Symbol):
-            return (
-                self.value == value.value
-                and self.type == value.type
-                and self.argument_count == value.argument_count
-                and self.precedence == value.precedence
-            )
-        return False
-
-    @override
-    def __str__(self) -> str:
-        return f"{self.value}"
-
-    @override
-    def __repr__(self) -> str:
-        return f"Symbol({self.value}, {self.type}, {self.argument_count})"
-
-
-binary_operators: dict[str, Symbol] = {
-    "*": Symbol("*", SymbolType.OPERATOR, 2, 2),
-    "/": Symbol("/", SymbolType.OPERATOR, 2, 2),
-    "%": Symbol("%", SymbolType.OPERATOR, 2, 2),
-    "+": Symbol("+", SymbolType.OPERATOR, 2, 1),
-    "-": Symbol("-", SymbolType.OPERATOR, 2, 1),
-}
-
-unary_operators: dict[str, Symbol] = {
-    "-": Symbol("-", SymbolType.OPERATOR, 1, 100),
-    "+": Symbol("+", SymbolType.OPERATOR, 1, 100),
-}
 
 
 class Evaluator:
@@ -207,6 +142,11 @@ class Evaluator:
         if len(output) != 1:
             raise ExpressionError(f"Expression led to no result {output}")
         return output.popleft()
+
+    def evaluate(self, code_line: str = "") -> float:
+        rpn = self._create_rpn_from(code_line)
+        result = self._solve_rpn(rpn)
+        return result
 
     @override
     def __str__(self) -> str:
