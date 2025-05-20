eb3014f8833de2744aab51810767b82a52d963a5
NxtPerfect <alakaxan@protonmail.com>
2025-05-20 07:42:56 +0200

    feat: added IfStatement interpreting

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,10 +1,11 @@
 from os.path import isdir, exists
 from pathlib import Path
+from typing import Callable
 
 from src.exceptions.FileEmptyError import FileEmptyError
 from src.constants import other
 from src.exceptions.FileWrongTypeError import FileWrongTypeError
-from src.joyTypes.AST_Nodes import Node, PrintStatement
+from src.joyTypes.AST_Nodes import IfStatement, Node, PrintStatement
 from src.parser import Parser
 from src.tokenizer import Tokenizer
 
@@ -34,6 +35,7 @@ def read_source_file(source_path: str):
         raise IOError
     return source_file
 
+
 def interpret(source_path: str) -> None:
     file_lines = read_source_file(source_path)
     tokenizer = Tokenizer()
@@ -43,11 +45,37 @@ def interpret(source_path: str) -> None:
     for node in nodes:
         visit_node(node)
 
+
 def visit_node(node: Node):
     if isinstance(node, PrintStatement):
-        # print
         print(node.text)
         return
+    if isinstance(node, IfStatement):
+        if check_condition(node):
+            if not node.body:
+                return
+            visit_node(node.body)
+            return
+        if not node.elseBody:
+            return
+        visit_node(node.elseBody)
+        return
     pass
+
+
+def check_condition(node: IfStatement) -> bool:
+    comparisons: dict[str, Callable[[float, float], bool]] = {
+        "<": lambda x, y: x < y,
+        ">": lambda x, y: x > y,
+        "<=": lambda x, y: x <= y,
+        ">=": lambda x, y: x >= y,
+        "==": lambda x, y: x == y,
+        "!=": lambda x, y: x != y,
+    }
+    return comparisons[node.condition.operator.token](
+        node.condition.left.value, node.condition.right.value
+    )
+
+
 #
 # run("examples/hello.joy")
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,5 +1,101 @@
-from src.interpreter import interpret
+from src.interpreter import check_condition, interpret, visit_node
+from src.joyTypes.AST_Nodes import (
+    Comparison,
+    EmptyExpression,
+    IfStatement,
+    PrintStatement,
+)
+from src.joyTypes.Token import Token, TokenType
 
 
-def test_print():
-    interpret("examples/hello.joy")
+def test_condition():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token("<", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        EmptyExpression(),
+        EmptyExpression(),
+    )
+    assert check_condition(node) == True
+
+
+def test_condition_false():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token(">", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        EmptyExpression(),
+        EmptyExpression(),
+    )
+    assert check_condition(node) == False
+
+
+def test_condition_equals():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token("==", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        EmptyExpression(),
+        EmptyExpression(),
+    )
+    assert check_condition(node) == False
+
+
+def test_condition_greater_equal():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token(">=", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        EmptyExpression(),
+        EmptyExpression(),
+    )
+    assert check_condition(node) == False
+
+
+def test_condition_less_equal():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token("<=", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        EmptyExpression(),
+        EmptyExpression(),
+    )
+    assert check_condition(node) == True
+
+
+def test_condition_body():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token("<", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        PrintStatement("We run the body"),
+        EmptyExpression(),
+    )
+    visit_node(node)
+    assert check_condition(node) == True
+
+
+def test_condition_else_body():
+    node = IfStatement(
+        Comparison(
+            Token("1", TokenType.NUMBER, 1),
+            Token(">", TokenType.COMPARISON_OPERATOR),
+            Token("2", TokenType.NUMBER, 2),
+        ),
+        EmptyExpression(),
+        PrintStatement("We run the else body"),
+    )
+    visit_node(node)
+    assert check_condition(node) == False
